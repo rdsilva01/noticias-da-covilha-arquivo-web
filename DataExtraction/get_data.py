8d55c99e8a1f083d39ee0fbcae5bb0a538c1e775
rodrigo <101207588+rdsilva01@users.noreply.github.com>
2024-02-26 17:46:27 +0000

    adding files

--- DataExtraction/get_data.py
+++ DataExtraction/get_data.py
@@ -2,18 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 from publicnewsarchive import dataExtraction
 import json
-import re
+import os
 
 def clean_string(input_string):
     cleaned_string = input_string.replace('\n', ' ')
+    cleaned_string = ' '.join(cleaned_string.split()) 
     cleaned_string = cleaned_string.strip()
 
     return cleaned_string
 
 
+
 def get_news_data(url, headers):
-  # debug
-  # print("url in use: ", url + "\n" + "*------------------"* 10 + "*")
   page = requests.get(url, headers=headers)
   soup = BeautifulSoup(page.text, 'html.parser')
 
@@ -35,8 +35,10 @@ def get_news_data(url, headers):
           span_regions = unique_news_div.find_all('span')
 
           if(len(span_regions) > 1):
-            paragraphs = span_regions[1] # paragraphs
-            full_paragraphs.append(paragraphs.text)
+            paragraphs = span_regions[1].find_all('p') # paragraphs
+            for paragraph in paragraphs:
+              # print(paragraph.text)
+              full_paragraphs.append(paragraph.text)
             if(len(span_regions) > 2):
               author = span_regions[2].text # author
               if(len(span_regions) > 3):
@@ -49,25 +51,25 @@ def get_news_data(url, headers):
           # print("author: ", author) # the author
           # print("tags: ", full_tags) # the full tags array
 
-          return([full_paragraphs, author, full_tags])
+          return [full_paragraphs, author, full_tags]
 
           # unique_news_div.find_all('span').find(class_='georgia')
 
     else:
       print("nested_div not found.")
-      return([])
+      return []
 
   else:
       print("news content not found.")
-      return([])
+      return []
 
-def get_news_page(url, headers):
+def get_news_page(url, headers, news_div_id):
   full_news_dict = []
 
-  page = requests.get(pastURLs[12], headers=headers)
+  page = requests.get(url, headers=headers)
   soup = BeautifulSoup(page.text, 'html.parser')
 
-  main_div = soup.find(id='div_conteudo_left')  # div that contains the news
+  main_div = soup.find(id=news_div_id)  # ARG 1
 
   total_news = 0
 
@@ -99,48 +101,38 @@ def get_news_page(url, headers):
 
               if text_region_text:
                 region = text_region_text.text
-                # region = clean_string(region_tmp)
-                # print("region: ", region)
                 date = text_date.text
-                # date = clean_string(date_tmp)
-                # print("date: ", date)
 
               text_title = text_element.find('h2') # 1 h2
               if text_title:
                 text_title_text = text_title.text # the title text
                 title = text_title_text
-                # title = clean_string(title_tmp)
-
-                # print("title: ", title)
 
               text_t_snippet = text_element.find('p') # 1 p
               if text_t_snippet:
                 text_t_snippet_text = text_t_snippet.text # t_snippet text
                 text_snippet = text_t_snippet_text
-                # text_snippet = clean_string(text_snippet_tmp)
-                # print("text snippet: ", text_snippet)
-
 
               image_link = image_element.find('a') # all the images are inside a link, that redirects to the news page
               if image_link:
                   image_src = image_link.find('img')
                   img_src = image_src.get('src')
-                  # print("img src: ", img_src) # image src
 
                   image_link_href =  image_link.get('href')
                   href = image_link_href
                   # print("href: ", href ) # link to the news page
 
                   news_list = get_news_data(image_link_href, headers) # in here will try to scrap the "same way" as we do to get the news pages
-                  paragraph = news_list[0]
+                  paragraph_tmp = news_list[0]
                   author_tmp = news_list[1]
-                  author = clean_string(author_tmp)
                   tags_tmp = news_list[2]
 
+                  author = clean_string(author_tmp)
+
                   # cleaning
-                  # for paragraph_s in paragraph_tmp:
-                  #   clean_paragraph = clean_string(paragraph_s)
-                  #   paragraph.append(clean_paragraph)
+                  for paragraph_s in paragraph_tmp:
+                    clean_paragraph = clean_string(paragraph_s)
+                    paragraph.append(clean_paragraph)
 
                   for tag in tags_tmp:
                     clean_tag = clean_string(tag)
@@ -163,31 +155,44 @@ def get_news_page(url, headers):
                   total_news += 1
 
                   full_news_dict.append(news_dict)
-                  # print(news_dict)
 
-                  # print("*------------------"* 10 + "*")
-
-
-      # print(full_news_dict)
-      # full_news_dict_json_string = json.dumps(full_news_dict, indent=4, ensure_ascii=False)
-      # print(full_news_dict_json_string)
-
-      # print("amount of news", total_news)
-
-      return [full_news_dict, total_news]
-
-      # with open("demo_data.json", "w") as file:
-      #  json.dump(full_news_dict, file, indent=4, ensure_ascii=False)
-
-      # print("data stored successfully!")
-
+      return full_news_dict
+      
   else:
-      print("div_conteudo_left not found.")
+      print("news not found at url: {}".format(url))
+      return []
+
+
+def get_x_month_news_data(pastURLs, year, headers, debug):
+
+  folder_name = "data_{}".format(year)
+  folder_path = "./data/{}".format(folder_name)
+
+  if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+  full_news_page = []
+
+  for i, pastURL in enumerate(pastURLs):
+        page = get_news_page(pastURL, headers, "div_conteudo_left")
+        if page != []:
+          full_news_page.append(page)
+          if debug:
+              if i != 0 and i % 1 == 0:
+                  print(f"\r{100 * i / len(pastURLs):.2f}%", end='')       
+                  if i == len(pastURLs) - 1:
+                      print(f"\r100.00%", end='')
+    
+  file_path = os.path.join(folder_path, "{}.json".format(year))
+  with open(file_path, "w") as file:
+    json.dump(full_news_page, file, indent=4, ensure_ascii=False)
+
+    
 
 
 
 # debugging
-pastURLs = dataExtraction.getPastURLs(year='2012', newspaper_url='https://noticiasdacovilha.pt/', startMonth='01', endMonth='01')
+# pastURLs = dataExtraction.getPastURLs(year='2012', newspaper_url='https://noticiasdacovilha.pt/', startMonth='01', endMonth='01')
 # print(len(pastURLs))
 
 # user agent string
@@ -195,15 +200,18 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 }
 
+url = "https://noticiasdacovilha.pt/"
 
-monthly_news = 0
-for i, pastURL in enumerate(pastURLs):
-  tmp_page = get_news_page(pastURL, headers)
-  monthly_news += tmp_page[1]
-  with open("{}.json".format(i), "w") as file:
-        json.dump(tmp_page[0], file, indent=4, ensure_ascii=False)
+# 2009
+# pastURLs = dataExtraction.getPastURLs(year=2009, newspaper_url=url, startMonth="01", endMonth="12" )
+# get_x_month_news_data(pastURLs, "2009", headers, True)
 
-  print("data stored successfully!")
-  print('amount of news at the moment: ', monthly_news)
 
-print("amount of news from month X: ", monthly_news)
+# 2010
+# pastURLs = dataExtraction.getPastURLs(year=2010, newspaper_url=url, startMonth="01", endMonth="12")
+# get_x_month_news_data(pastURLs, "2010", headers, True)
+
+
+# 2011
+pastURLs = dataExtraction.getPastURLs(year=2011, newspaper_url=url, startMonth="01", endMonth="01")
+get_x_month_news_data(pastURLs, "2011", headers, True)
