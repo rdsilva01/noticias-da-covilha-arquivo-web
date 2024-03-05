3a5a57be848ad596bf4e47c48cd761644fe736d7
rodrigo <101207588+rdsilva01@users.noreply.github.com>
2024-03-05 13:12:36 +0000

    Update data_validation.py

--- DataValidation/data_validation.py
+++ DataValidation/data_validation.py
@@ -1,8 +1,25 @@
-import json
 import os
+import json
+from datetime import datetime
 
 def sort_data(input_file, output_file):
-    print()
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+
+    sorted_articles = []
+
+    for article in data:
+        if "date" in article:
+            sorted_articles.append(article)
+
+    sorted_articles.sort(key=lambda x: x.get("date"))
+
+    # for i, article in enumerate(sorted_articles):
+    #     print("{} - {}".format(i, article["date"]))
+
+    with open(output_file, "w") as outfile:
+        json.dump(sorted_articles, outfile, indent=4, ensure_ascii=False)
+   
 def data_validation(s_year, e_year):
     for year in range(s_year, e_year + 1):
         input_file = f'./data/data_{year}/{year}.json'
